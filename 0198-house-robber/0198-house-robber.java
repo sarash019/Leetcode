b0c884e155b8769383f207ba1a4a069f0464ad47
Sarash Singh <sarashsingh019@gmail.com>
2026-09-12 15:09:26 +0530

    Time: 0 ms (100%), Space: 42.9 MB (19.33%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int rob(int[] nums) {
+        //tabulation with best space
+        int prev = nums[0];
+        int prev2 = 0;
+        for(int i = 0; i < nums.length; i++){
+            int take = nums[i];
+            if(i > 1)  take += prev2;
+
+            int nottake = 0 + prev;
+
+            int curri = Math.max(take, nottake);
+
+            prev2 = prev;
+            prev = curri;
+        }
+        return prev;
+    }
+}
